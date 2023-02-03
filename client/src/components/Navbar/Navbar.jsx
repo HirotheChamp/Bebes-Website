930e7bfb0d6a565bacc2c5b0913a097e05e2f961
Kyle Hiroshi Young <93508000+HirotheChamp@users.noreply.github.com>
2023-02-02 17:39:59 -0800

    newest commit

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -5,13 +5,13 @@ const Navbar = () => {
   return (
     <header className='navbar'>
   <div className='navbar-logo'>
-<h1>SeRi Photography</h1>
+<a href="/"><h1>SeRi Photography</h1></a>
 </div>
 
       <nav className='navbar-items'>
     
         <ul>
-            <li><a href="/">Home</a></li>
+            
             <li><a href="/services">Services</a></li>
             <li><a href="/contact">Contact Me</a></li>
             <li><a href="/about">About Me</a></li>
