3f8c1bb19ee4e9618c63c9416e060929a189d9ca
Kyle Hiroshi Young <93508000+HirotheChamp@users.noreply.github.com>
2023-03-07 11:04:24 -0800

    started photo gallery

--- client/src/pages/Portraits/Portraits.jsx
+++ client/src/pages/Portraits/Portraits.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Portraits = () => {
   return (
-    <div>Portraits</div>
+    <div className='portraits'>
+      <h1>Portraits</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
   )
 }
 
