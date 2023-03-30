39ca25eb7ce955b2787173c4712628a5a01c8767
Kyle Hiroshi Young <93508000+HirotheChamp@users.noreply.github.com>
2023-03-29 20:03:27 -0700

    added framer motion and fixed css

--- client/src/components/ProgressBar/ProgressBar.js
+++ client/src/components/ProgressBar/ProgressBar.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import useStorage from '../../hooks/useStorage'
 import './ProgressBar.css'
+import { motion } from 'framer-motion'
 
 const ProgressBar = ({file, setFile}) => {
     const {url, progress} = useStorage(file);
@@ -12,7 +13,10 @@ if (url) {
     }, [url, setFile])
    
   return (
-    <div className='progress-bar' style={{width: progress + '%'}}></div>
+    <motion.div className='progress-bar' 
+    initial={{ width: 0 }}
+    animate={{ width: progress + '%' }}
+    ></motion.div>
   )
 }
 
