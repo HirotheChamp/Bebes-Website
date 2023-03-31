b6204526169423e9c2ecff273a6e30a7792c821e
Kyle Hiroshi Young <93508000+HirotheChamp@users.noreply.github.com>
2023-03-31 13:13:12 -0700

    added component image grid to all supported pages

--- client/src/pages/Commercial/Commercial.jsx
+++ client/src/pages/Commercial/Commercial.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './Commercial.css'
+import ImageUpload from '../../components/Image Upload/ImageUpload';
+import ImageGrid from '../../components/ImageGrid/ImageGrid'
+import Modal from '../../components/Modal/Modal';
 
 const Commercial = () => {
+  const [selectedImg, setSelectedImg] = useState(null);
   return (
-    <div>Commercial</div>
+    <div className='commercial'>
+      <h1>Commercial</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+      <ImageUpload />
+
+      <ImageGrid setSelectedImg={setSelectedImg}/>
+      { selectedImg && <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg}/> }
+      </div>
   )
 }
 
--- client/src/pages/Sports/Sports.jsx
+++ client/src/pages/Sports/Sports.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Sports.css'
+import ImageUpload from '../../components/Image Upload/ImageUpload';
+import ImageGrid from '../../components/ImageGrid/ImageGrid'
+import Modal from '../../components/Modal/Modal';
 
 const Sports = () => {
+  const [selectedImg, setSelectedImg] = useState(null);
   return (
-    <div>Sports</div>
+    <div className='sports'>
+      <h1>Sports</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+      <ImageUpload />
+
+      <ImageGrid setSelectedImg={setSelectedImg}/>
+      { selectedImg && <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg}/> }
+      </div>
   )
 }
 
